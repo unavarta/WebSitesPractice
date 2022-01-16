0abbfb775d31f7ec150b2c4f8163b1dc96216f6f
Uriel Navarta <unavarta@gmail.com>
2022-01-15 21:01:40 -0300

    Pagina actualizada con Links de IG, WPP y Correo.

---
+++ hola-uriel/script.js
@@ -0,0 +1,17 @@
+document.body.addEventListener("mousemove", event =>{
+
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    gsap.set(".cursor", {
+        x: mouseX,
+        y:mouseY,
+    })
+
+    gsap.to(".shape", {
+        x: mouseX,
+        y: mouseY,
+
+        stagger: -0.1
+    })
+});
